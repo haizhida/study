d790ddbf2fa91d14dfcf636f1e355326be12f795
lee <3271113119@qq.com>
2019-09-22 15:56:54 +0800

    第三次 比较在哪里

--- test/src/cn/itcast/test.java
+++ test/src/cn/itcast/test.java
@@ -3,6 +3,9 @@ package cn.itcast;
 public class test {
     public static void main(String[] args) {
         System.out.println("你好 git");
+        int  u=0;
+        System.out.println(u);
+
     }
     public  void t(){
         System.out.println("方法执行");
