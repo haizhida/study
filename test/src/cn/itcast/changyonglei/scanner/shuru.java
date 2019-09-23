d8ad152fe1b37fe87670c918675264819b226117
lee <3271113119@qq.com>
2019-09-23 15:34:58 +0800

    增加scanner

---
+++ test/src/cn/itcast/changyonglei/scanner/shuru.java
@@ -0,0 +1,12 @@
+package cn.itcast.changyonglei.scanner;
+
+import java.util.Scanner;
+
+public class shuru {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("输入一个整数");
+        int i = scanner.nextInt();
+        System.out.println(i);
+    }
+}
